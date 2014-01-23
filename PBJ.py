62ad144766a7e11de6aec28513c1eee109363723
shinson <sonia.hinson@gmail.com>
2014-01-22 22:26:14 -0500

    Update PBJ.py

--- PBJ.py
+++ PBJ.py
@@ -5,107 +5,92 @@ jelly = random.randrange(0, 10+1)
 bread = random.randrange(0, 10+1)
 
 if bread == 1:
-	Nobread = "one slice"
+	No_of_bread = "one slice"
 elif bread >1:
-	Nobread = "{0} slices".format(bread)
+	No_of_bread = "{0} slices".format(bread)
 else:
-	Nobread = " 0 slices."
+	No_of_bread = " 0 slices."
 
-if pb >=1 and jelly >=1 and bread >=2:
-	PBJsandwich = "Yes"
+No_bread_pairs = bread/2
+
+if No_bread_pairs <= pb and No_bread_pairs < jelly:
+	No_of_sandwiches = No_bread_pairs
+elif pb <= jelly and pb < No_bread_pairs:
+	No_of_sandwiches = pb
+elif jelly < pb and jelly <= No_bread_pairs:
+	No_of_sandwiches = jelly
+elif No_bread_pairs == 0 or pb == 0 or jelly == 0:
+	No_of_sandwiches = 0
 else:
-	PBJsandwich = "No"
-
-NoOfBread = bread/2
-
-if NoOfBread < pb and NoOfBread < jelly:
-	NoOfSandwiches = NoOfBread
-elif pb < jelly and pb < NoOfBread:
-	NoOfSandwiches = pb
-elif pb > jelly and jelly < NoOfBread:
-	NoOfSandwiches = jelly
-elif pb == jelly and pb<NoOfBread:
-	NoOfsandwiches = pb
-elif NoOfBread == pb and NoOfBread<jelly:
-	NoOfSandwiches = NoOfBread
-elif jelly == NoOfBread and jelly < pb:
-	NoOfSandwiches = jelly
-elif NoOfBread == 0 or pb == 0 or jelly == 0:
-	NoOfSandwiches = 0
-else:
-	NoOfSandwiches = NoOfBread
+	No_of_sandwiches = 0
 	
-if NoOfSandwiches == 1 :
+if No_of_sandwiches == 1 :
 	part1 = "1 PB&J sandwich. Hip Hip Hooray."
-elif NoOfSandwiches >1 :
-	part1 = "{0} PB&J sandwiches! Lunch will be awesome.".format(NoOfSandwiches)
+elif No_of_sandwiches >1 :
+	part1 = "{0} PB&J sandwiches! Lunch will be awesome.".format(No_of_sandwiches)
 else:
 	part1 = "0 PB&J sandwiches :-(. Let's see the alternatives!"
 
-openFace = bread%2
-leftoverJ = jelly - NoOfSandwiches
-leftoverPB = pb - NoOfSandwiches
+open_face = bread%2
+leftoverJ = jelly - No_of_sandwiches
+leftoverPB = pb - No_of_sandwiches
 
-if openFace == 1 and leftoverJ>=1 and leftoverPB>=1:
-	NoOpenFace = 1
+if open_face == 1 and leftoverJ>=1 and leftoverPB>=1:
+	No_of_open_face = 1
 elif bread == 0 or jelly == 0 or pb == 0:
-	NoOpenFace = 0
+	No_of_open_face = 0
 else:
-	NoOpenFace = 0
+	No_of_open_face = 0
 
-if NoOpenFace == 1 :
+if No_of_open_face == 1 :
 	part2 = "1 PB&J open face sandwich. Not the best, but just deal with it."
 else:
 	part2 = "0 PB&J  open face sandwiches. There must be other sandwiches"
 
-leftoverJ2 = jelly - NoOfSandwiches - NoOpenFace
-leftoverPB2 = pb - NoOfSandwiches - NoOpenFace
-leftoverB2 = bread - (NoOfSandwiches*2) - NoOpenFace
-leftoverNoOfBread = leftoverB2/2
+leftoverJ2 = jelly - No_of_sandwiches - No_of_open_face
+leftoverPB2 = pb - No_of_sandwiches - No_of_open_face
+leftoverB2 = bread - (No_of_sandwiches*2) - No_of_open_face
+leftover_no_of_bread= leftoverB2/2
 
 
-if leftoverJ2 < leftoverNoOfBread and leftoverB2>=2 and leftoverJ2 >=1:
-	noJellySandwich = leftoverJ2
-elif leftoverJ2 > leftoverNoOfBread and leftoverB2>=2:
-	noJellySandwich = leftoverNoOfBread
-elif leftoverJ2==0 or leftoverNoOfBread<=2:
-	noJellySandwich = 0
-elif leftoverJ2 == leftoverNoOfBread:
-	noJellySandwich = leftoverJ2
+if leftoverJ2 <= leftover_no_of_bread and leftoverB2>=2 and leftoverJ2 >=1:
+	no_of_jelly_sandwich = leftoverJ2
+elif leftoverJ2 > leftover_no_of_bread and leftoverB2>=2:
+	no_of_jelly_sandwich = leftover_no_of_bread
+elif leftoverJ2==0 or leftover_no_of_bread<=2:
+	no_of_jelly_sandwich = 0
 else:
-	noJellySandwich = 0
+	no_of_jelly_sandwich = 0
 
-if noJellySandwich == 1 :
+if no_of_jelly_sandwich == 1 :
 	part3 = "1 jelly sandwich Good...I guess."
-elif noJellySandwich >1 :
-	part3 = "{0} Jelly sandwiches! I hope you like jelly.".format(noJellySandwich)
+elif no_of_jelly_sandwich >1 :
+	part3 = "{0} Jelly sandwiches! I hope you like jelly.".format(no_of_jelly_sandwich)
 else:
 	part3 = "0 jelly sandwiches. Peanut butter is better anyways."
 
-if leftoverPB2 < leftoverNoOfBread and leftoverPB2>=2 and leftoverPB2>=1:
-	noPbSandwich = leftoverJ2
-elif leftoverPB2 > leftoverNoOfBread and leftoverPB2>=2:
-	noPbSandwich = leftoverNoOfBread
-elif leftoverPB2==0 or leftoverNoOfBread<=2:
-	noPbSandwich = 0
-elif leftoverPB2==leftoverNoOfBread:
-	noPbSandwich = leftoverPB2
+if leftoverPB2 <= leftover_no_of_bread and leftoverPB2>=2 and leftoverPB2>=1:
+	no_of_pb_sandwich = leftoverJ2
+elif leftoverPB2 > leftover_no_of_bread and leftoverPB2>=2:
+	no_of_pb_sandwich = leftover_no_of_bread
+elif leftoverPB2==0 or leftover_no_of_bread<=2:
+	no_of_pb_sandwich = 0
 else:
-	noPbSandwich = 0
+	no_of_pb_sandwich = 0
 
-if noPbSandwich == 1 :
+if no_of_pb_sandwich == 1 :
 	part4 = "1 peanut butter sandwich for you. Good...I guess."
-elif noPbSandwich >1 :
-	part4 = "{0} peanut butter sandwiches! I hope you like peanut butter.".format(noPbSandwich)
+elif no_of_pb_sandwich >1 :
+	part4 = "{0} peanut butter sandwiches! I hope you like peanut butter.".format(no_of_pb_sandwich)
 else:
 	part4 = "0 peanut butter sandwiches. Aww shucks."
 
-totalSandwich = NoOfSandwiches + NoOpenFace + noJellySandwich + noPbSandwich
+total_sandwich = No_of_sandwiches + No_of_open_face + no_of_jelly_sandwich + no_of_pb_sandwich
 
-if totalSandwich == 1:
+if total_sandwich == 1:
 	part5 = "Only 1 sandwich today. I hope it fills you up!"
-elif totalSandwich >1:
-	part5 = "{0} sandwiches today for lunch! I hope you're hungry.".format(totalSandwich)
+elif total_sandwich >1:
+	part5 = "{0} sandwiches today for lunch! I hope you're hungry.".format(total_sandwich)
 else:
 	part5 = "No sandwiches for you today. It's time for take out!"
 
@@ -116,5 +101,5 @@ Therefore you can make {3}
 You can make {4}
 And make {5}
 Finally, you can make {6}
-{7}""".format(pb, jelly, Nobread, part1, part2, part3, part4, part5)
+{7}""".format(pb, jelly, No_of_bread, part1, part2, part3, part4, part5)
 
